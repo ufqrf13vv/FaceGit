d8f70e42bf79369536b2ea6cd71f2d0f1171c8ad
Artem Samofalov <dex157@gmail.com>
2018-04-24 13:12:10 +0500

    init commit

---
+++ scripts/create_user_branches.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+const exec = require('child_process').exec;
+
+const execPromise = command =>
+  new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => (err ? reject(err) : resolve(stdout, stderr)));
+  });
+
+const students = [
+  'dashko',
+  'isaev',
+  'ivanov',
+  'kontareva',
+  'kulakouski',
+  'kurbakov',
+  'lobastov',
+  'marsov',
+  'nikitin',
+  'noschenko',
+  'pervushin',
+  'pogorelyi',
+  'polukarpov',
+  'polyansky',
+  'ragulin',
+  'reshetnev',
+  'samsonova',
+  'suleimanov',
+  'teterin',
+  'uriev',
+];
+
+async function main() {
+  for (let student of students) {
+    await createHomeworkBranch(student);
+  }
+  await execPromise('git checkout master');
+}
+
+async function createHomeworkBranch(student) {
+  let homeworkName = `${student}/homeproject`;
+  await execPromise(`git checkout master`);
+  await execPromise(`git checkout -b ${homeworkName}`);
+  await execPromise(`git push --set-upstream origin ${homeworkName}`);
+}
+
+main()
+  .catch(error => {
+    console.log(error);
+  })
+  .then(() => {
+    process.exit(0);
+  });
